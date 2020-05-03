e2684df426727ec6719a79878839f0770fd846ca
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-05-03 16:55:45 -0300

    adding reddit instance for bot use on test route

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+import os
+
 from flask import Flask
 from flask import jsonify
+
+import praw
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
 app = Flask(__name__)
@@ -8,6 +15,33 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def index():
     return jsonify({"message": "Goodbye world!"})
+
+
+@app.route('/test', methods=["GET"])
+def test():
+
+    print(os.getenv("CLIENT_ID"))
+    print(os.getenv("CLIENT_SECRET"))
+    print(os.getenv("USER_AGENT"))
+
+
+    reddit = praw.Reddit(client_id=os.getenv("CLIENT_ID"),
+                         client_secret=os.getenv("CLIENT_SECRET"),
+                         user_agent=os.getenv("USER_AGENT"),
+                         username=os.getenv("USERNAME"),
+                         password=os.getenv("PASSWORD"))
+
+    for i in dir(reddit):
+        print("Reddit param: ", i)
+
+    print(help(reddit.redditor))
+
+    print(reddit.read_only)
+
+
+
+    return jsonify({"message":"this is a test endpoint."})
+
 
 
 if __name__ == "__main__":
